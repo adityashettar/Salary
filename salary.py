9a2fd5d42ebfb81fbde8b5f2d9b5d33c5b966982
adityashettar <adityashettar01@gmail.com>
2025-11-10 16:12:54 +0530

    Fix print statement formatting for total salary

--- salary.py
+++ salary.py
@@ -3,4 +3,4 @@ salary = 10000
 bonus = salary * 0.10
 total_salary = salary + bonus
 print("Bonus Amount: ", bonus)
-print("Total Salary after Bonus: ₹",total_salary)
+print("Total Salary after Bonus: ",total_salary)
