56107d697d7badbb5cf3043f2d45daef11811044
akash <01fe24ica063@kletech.ac.in>
2025-11-10 14:40:22 +0530

    Save

---
+++ Studentgrade.py
@@ -0,0 +1,23 @@
+marks1 = 40
+marks2 = 60
+marks3 = 80
+marks4 = 95
+marks5 = 100
+
+
+sum=marks1+marks2+marks3+marks4+marks5
+avg=sum/5
+
+if avg >= 90:
+    print("Grade: A")
+elif avg >= 80:
+    print("Grade: B")
+elif avg >= 70:
+    print("Grade: C")
+elif avg >= 60:
+    print("Grade: D")
+elif avg >= 50:
+    print("Grade: E")
+else :
+    print("Grade: F")
+
