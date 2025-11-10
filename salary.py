55b2b2ee6d0a6b8b65d9a75914ceeae8ae37c09f
adityashettar <adityashettar01@gmail.com>
2025-11-10 16:11:45 +0530

    Update salary.py

--- salary.py
+++ salary.py
@@ -1,14 +1,6 @@
-# Program to calculate 10% bonus and total salary
 
-# Take salary input from the user
-salary = float(input("Enter the employee's salary: "))
-
-# Calculate bonus (10% of salary)
-bonus = 0.10 * salary
-
-# Calculate total salary
+salary = 10000
+bonus = salary * 0.10
 total_salary = salary + bonus
-
-# Display the results
-print("Bonus Amount: ₹", bonus)
-print("Total Salary: ₹", total_salary)
+print("Bonus Amount: ", bonus)
+print("Total Salary after Bonus: ₹",total_salary)
